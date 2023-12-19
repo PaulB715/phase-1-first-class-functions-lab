17610ccef5d98c818042cb98b0f48671f002d734
Paul Brown <paulbrown389@gmail.com>
2023-12-18 19:24:43 -0500

    First Class Lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(2, 4)
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  }
+  function fareDoubler(fare) {
+       return fare * 2
+  }
+  function fareTripler(fare) {
+    return fare * 3
+  }
+  function selectDifferentDrivers(arrayOfDrivers, driverSelectorFunction) {
+    return driverSelectorFunction(arrayOfDrivers);
+  }
+  
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+  
